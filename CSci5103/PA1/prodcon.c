615ece147353e1f3b194dbcc2b05ea2cde4231da
Matthew Le <lematt@cs.umn.edu>
2012-09-19 11:38:27 -0500

    working without any calls to sleep

--- CSci5103/PA1/prodcon.c
+++ CSci5103/PA1/prodcon.c
@@ -1,7 +1,5 @@
 #include "prodcon.h"
 
-#define SHMKEY 2041
-#define SHMKEY2 2042
 sigset_t sig_set;
 struct sharedMemStruct{
   char * data;
@@ -11,7 +9,7 @@ struct sharedMemStruct{
 void sigHandler(int n){
   signal(n, sigHandler);
 }
-void producerWriteText(char * filePath, pid_t childPID){
+void producerWriteText(char * filePath, pid_t childPID, char* data, int* numCharsPointer){
   
   //open the file and check that it exists
   FILE *inputFile = fopen(filePath, "r");
@@ -19,27 +17,14 @@ void producerWriteText(char * filePath, pid_t childPID){
     printf("input file, \"%s\" does not exist, exiting...\n", filePath);
     exit(1);
   }
-  //--------------Get Shared Memory-----------
-  int sharedMemId, charsReadId;
-  sharedMemId = shmget(SHMKEY, 1024, 0666 | IPC_CREAT );
-  charsReadId = shmget(SHMKEY2, 4, 0666 | IPC_CREAT);
-  if(sharedMemId == -1 || charsReadId == -1){
-    perror("Error creating shared memory.\n");
-    exit(1);
-  }
   
   struct sharedMemStruct sharedMem;
-  sharedMem.numChars = shmat(charsReadId, 0, 0);
-  sharedMem.data = shmat(sharedMemId, 0, 0);
-  if(sharedMem.data == (char*) -1 || sharedMem.numChars == (int*) -1){
-    perror("Error attaching shared memory.\n");
-    exit(1);
-  }
+  sharedMem.numChars = numCharsPointer;
+  sharedMem.data = data;
+  
   char *currentBlock = (char*)malloc(sizeof(char) * 1024);
   int charsRead = fread(currentBlock, 1, 1024, inputFile);
   int j = 0;
-  printf("waiting for child to setup\n");
-  sleep(3);
   //-----Read in chars and write to Shared memory------
   while(charsRead > 0){
     int i;
@@ -52,7 +37,6 @@ void producerWriteText(char * filePath, pid_t childPID){
     printf("writing %d chars to shared memory\n", charsRead);
     
     kill(childPID, SIGUSR1); //send SIGUSR1
-    //printf("waiting on signal SIGUSR2\n");
     sigpause(SIGUSR2); //Wait for SIGUSR2
 
     j = j+1;
@@ -63,42 +47,38 @@ void producerWriteText(char * filePath, pid_t childPID){
   kill(childPID, SIGUSR1);
   shmdt((void*) sharedMem.data);
   free(currentBlock);
-  //printf("parent exiting...\n");
 }
 
-void consumerReadText(){
+void consumerReadText(key_t dataKey, key_t numCharsKey){
 
-  sleep(1);
   FILE *outputFile = fopen("output", "w");
   if(outputFile == NULL){
     perror("Error opening output file\n");
     exit(0);
   }
-  //printf("getting shared memory to consumer\n");
   //-----------Get Shared Memory-----------------
   int sharedMemId, charsReadId;
-  sharedMemId = shmget(SHMKEY, 1024, 0666);
-  charsReadId = shmget(SHMKEY2, 4, 0666);
+  sharedMemId = shmget(dataKey, 0, 0);
+  charsReadId = shmget(numCharsKey, 0, 0);
   if(sharedMemId == -1 || charsReadId == -1){
     perror("Error getting shared memory to consumer.\n");
     exit(0);
   }
   
+  int flag = 00400| 00200 | 00040 | 00020 | 00004 | 00002 ;
+  
   struct sharedMemStruct sharedMem;
-  sharedMem.data = shmat(sharedMemId, 0, 0);
-  sharedMem.numChars = shmat(charsReadId, 0, 0);
+  sharedMem.data = shmat(sharedMemId, (void*) NULL, flag);
+  sharedMem.numChars = shmat(charsReadId, (void*)NULL, flag);
   if(sharedMem.data == (char*) -1 || sharedMem.numChars == (int*) -1){
     perror("Error attaching shared memory to consumer.\n");
     exit(1);
   }
   //---Read in from shared memory and write to output----
   while(*sharedMem.numChars != -1){
-    //printf("waiting on signal SIGUSR1\n");
+    kill(getppid(), SIGUSR2);
     sigpause(SIGUSR1); //Wait for SIGUSR1
 
-    //printf("consumer recieved = \"%s\"\n", sharedMem.data);
-    //printf("consumer recieved numChars = \"%d\"\n", *sharedMem.numChars);
-    
     char *tempString = (char*)malloc(sizeof(char) * (*sharedMem.numChars) + 1);
     int i;
     for(i = 0; i < (*sharedMem.numChars); i++){
@@ -106,21 +86,16 @@ void consumerReadText(){
     }
     tempString[i+1] = '\0';
     if((*sharedMem.numChars) >=0){
-      //fprintf(outputFile, "%s", tempString);
       fwrite(sharedMem.data, sizeof(char) * 1, sizeof(char) * (*sharedMem.numChars), outputFile);
     }
     
     free(tempString);
-    //printf("sending signal SIGUSR2\n");
-    sleep(1);
     kill(getppid(), SIGUSR2); //Send SIGUSR2
   }
-  //printf("child leaving while loop\n");
   fclose(outputFile);
   *sharedMem.numChars = 0;
   shmdt((void*) sharedMem.data);
   shmctl(sharedMemId, IPC_RMID, NULL);
-  //printf("exiting...\n");
   exit(0);
   
 }
@@ -136,13 +111,50 @@ int main(int argc, char ** argv){
   signal(SIGUSR1, sigHandler);
   signal(SIGUSR2, sigHandler);
   
+  key_t key = 4455;
+  key_t key2 = 4457;
+  int size = 1024;
+  int flag = 0;
+  flag = flag | IPC_CREAT;
+  flag = flag| 00400 |00200 |00040 | 00020| 00004 | 00002 ;
+  int shmem_id;
+  char* shmem_ptr;
+  
+  shmem_id = shmget(key, size, flag);
+  if(shmem_id == -1){
+    perror("shmget failed data memory\n");
+    exit(1);
+  } 
+  shmem_ptr = shmat(shmem_id, (void*) NULL, flag);
+  if(shmem_ptr == (void*) -1){
+    perror("shmat failed for data memory\n");
+    exit(1);
+  }
+  
+  int shmem_id2 = shmget(key2, 4, flag);
+  if(shmem_id2 == -1){
+    perror("shmget failed for numChars memory\n");
+    exit(1);
+  } 
+  int *shmem_ptr2 = shmat(shmem_id2, (void*) NULL, flag);
+  if(shmem_ptr2 == (void*) -1){
+    perror("shmat failed for numChars memory\n");
+    exit(1);
+  }
+  
+  sigset_t sigs;
+  sigemptyset(&sigs);
+  sigaddset(&sigs, SIGUSR1);
+  
+  
   pid_t childPid = fork(); //fork a child process
   
   if(childPid == 0){ //child process goes to read function
-    consumerReadText();
+    consumerReadText(key, key2);
   }
   else if(childPid > 0){ //parent process
-    producerWriteText(filePath, childPid);
+    sigpause(SIGUSR2);
+    producerWriteText(filePath, childPid, shmem_ptr, shmem_ptr2);
     wait(&status);
   }
   else{
--- CSci5103/PA1/prodcon.h
+++ CSci5103/PA1/prodcon.h
@@ -3,6 +3,8 @@
 #include <sys/shm.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
 
 
 
