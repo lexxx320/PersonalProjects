a89cb7b7a4b64573367241cb3fefc0ca1085719a
Matthew Le <lexxx320@umn.edu>
2012-09-24 17:35:32 -0500

    prolog sublist is now working

--- CSci5106/Assignment1/prologSublist.pl
+++ CSci5106/Assignment1/prologSublist.pl
@@ -1,13 +1,8 @@
-sublist([], L).
+sublist([], _).
 sublist([X|L1], [X|L2]) :- sublist(L1, L2).
+sublist([X|L1], [_|L2]) :- sublist([X|L1], L2).
 
-quick_sort([],[]).
-quick_sort([H|T],Sorted):-
-	pivoting(H,T,L1,L2),quick_sort(L1,Sorted1),quick_sort(L2,Sorted2),
-	append(Sorted1,[H|Sorted2]).
-   
-pivoting(H,[],[],[]).
-pivoting(H,[X|T],[X|L],G):-X=<H,pivoting(H,T,L,G).
-pivoting(H,[X|T],L,[X|G]):-X>H,pivoting(H,T,L,G).
+sublist(L, [1, 2,3, 4, 5]).
+
 
 
