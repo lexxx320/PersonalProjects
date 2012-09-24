8b9ac5c46e26461d42afbe7801c54f5434ff99f5
Matthew Le <lexxx320@umn.edu>
2012-09-24 17:21:37 -0500

    scheme sublist now working

--- CSci5106/Assignment1/schemeSublist.scm
+++ CSci5106/Assignment1/schemeSublist.scm
@@ -1,4 +1,37 @@
-(define (odd x)
-  (if (=(modulo x 2) 0) #f #t))
+(define (removeK list k)
+  (define (helper list k firstPart)
+  (if (null? list) list
+      (if (= k 1) (append firstPart (cdr list))
+          (helper(cdr list) (- k 1) (cons (car list) firstPart))
+          )
+      )
+    )
+  (helper list k '())
+  )
 
-(define odd #t)
+(define (sublist listA listB)
+  (define (sublistHelper listA listB tempListB k)
+    (if (null? tempListB) #f
+        (if (= (car listA) (car tempListB)) (sublist (cdr listA) (removeK listB k))
+            (sublistHelper listA listB (cdr tempListB) (+ k 1))
+            )
+        )
+    )   
+  (if (null? listA) #t
+      (if (null? listB) #f
+          (sublistHelper listA listB listB 1)
+          )
+      )
+  )
+        
+        
+        
+  (sublist '(1 2 3 4) '(1 2 3 3 4 5))
+  (sublist '(1 1 1 1 1) '(1))
+  (sublist '(1 2 3 4) '(1 2 3 4))
+  (sublist '(1 2) '(1 2 3))
+  (sublist '(1 2) '(1 2 2 2 2))
+
+
+
+          
