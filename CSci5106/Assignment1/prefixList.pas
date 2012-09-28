09e57ae23ef412f1ea015c680e62e3e06805bc10
Matthew Le <lexxx320@umn.edu>
2012-09-28 11:31:27 -0500

    updating

--- CSci5106/Assignment1/prefixList.pas
+++ CSci5106/Assignment1/prefixList.pas
@@ -11,38 +11,46 @@ program prefix;
     num : Integer;
     code : Integer;
     result : Boolean;
+    //Prefix Function
   function prefix(listA : IntArray; sizeA : Integer; listB : IntArray; sizeB : Integer) : boolean;
   var
     i : Integer;
+    n : Integer;
   begin
+    writeln('Entering prefix function');
+    writeln(sizeA);
+    writeln(sizeB);
     prefix := true;
-    for i := 0 to sizeA do
-    begin
-      if listA[i] <> listB[i] then 
-        prefix := false;
+    n := sizeA - 1;
+    for i := 0 to n do
+      begin
+        writeln(listA[i]);
+        writeln(listB[i]);
+        if listA[i] <> listB[i] then 
+          prefix := false;
     end;
   end;
-  function readList(list : IntArray) : Integer
+  //Read list function
+  function readList(list : IntArray) : Integer;
   var
     i : Integer;
   begin
     i := 0;
     writeln('Please enter a list element by element terminated by a negative number');
     repeat
-      writeln('reading number');
-      read(tempString);
+      readln(tempString);
       Val(tempString, num, code);
-      writeln(code);
       if code <> 0 then
       begin
         writeln('Error, one of the numbers was not a valid number');
         Exit;
       end;
-      listA[i] := num;
+      list[i] := num;
       i := i+1;
-    until(num < 0)
-    readList = i-1;
+    until(num < 0);
+    readList := i-1;
   end;
+  //Main Function
   Begin
     
     sizeA := readList(listA);
@@ -51,7 +59,7 @@ program prefix;
     if result = true then 
       writeln('The first list is the prefix of the second list')
     else 
-      writeln('The first list is not the prefix of the secod list')
+      writeln('The first list is not the prefix of the second list')
 
 
     
--- CSci5106/Assignment1/prologSublist.pl
+++ CSci5106/Assignment1/prologSublist.pl
@@ -1,6 +1,25 @@
-sublist([], _).
-sublist([X|L1], [X|L2]) :- sublist(L1, L2).
-sublist([X|L1], [_|L2]) :- sublist([X|L1], L2).
+/*******************************************************
+Name: Matthew Le
+Student Id: 3975089
+x500: lexxx320
+
+Program Description: This program uses its first rule to 
+pass in the first list as the third argument to the rest of
+the rules.  The first rule for sublistHelper checks if the
+first list is empty.  The second rule checks to see if the
+head of both current lists are equal, if they are then check 
+for the rest of each list.  For the last rule, if the heads of 
+both lists are not equal, then the first list gets replaced with
+its original list and it starts over again.
+*******************************************************/
+
+
+sublist(L, L2) :- sublistHelper(L, L2, L).
+
+sublistHelper([], _, _).
+sublistHelper([X|L1], [X|L2], L3) :- sublistHelper(L1, L2, L3).
+sublistHelper([X|L1], [_|L2], L3) :- sublistHelper(L3, L2, L3).
+
 
 
 
