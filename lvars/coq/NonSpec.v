57446fe03736229fba434f8a7c2d111a2a1eee36
Matthew Le <lexxx320@umn.edu>
2014-03-18 15:01:41 -0400

    updating

--- lvars/coq/NonSpec.v
+++ lvars/coq/NonSpec.v
@@ -1,6 +1,7 @@
 
-Inductive id : Type := 
-  |Id : nat -> id. 
+Require Export SfLib. 
+
+Definition id := SfLib.id.
 
 (*Syntax*)
 Inductive term : Type := 
@@ -13,25 +14,47 @@ Inductive term : Type :=
   |bind : term -> term -> term
   |fork : term -> term
   |new : term
-  |put : id -> term
+  |put : id -> term -> term
   |get : id -> term
   |done : term -> term.
 
-Definition heap := nat.
+
+Inductive ivar_state : Type :=
+  |empty : ivar_state
+  |full : term -> ivar_state.
+
+
+Definition heap := partial_map ivar_state.
 
 Inductive pool : Type :=
   |thread : term -> pool
   |parComp : pool -> pool -> pool.
 
-
 Inductive step : heap -> pool -> heap -> pool -> Prop :=
-  |ParStep1 : forall t1 t1' t2 h h', step h t1 h' t1' -> step h (parComp t1 t2) h' (parComp t1' t2)
-  |ParStep2 : forall t1 t2 t2' h h', step h t2 h' t2' -> step h (parComp t1 t2) h' (parComp t1 t2')
+  |ParStep1 : forall t1 t1' t2 h h', step h t1 h' t1' -> 
+                                     step h (parComp t1 t2) h' (parComp t1' t2)
+  |ParStep2 : forall t1 t2 t2' h h', step h t2 h' t2' -> 
+                                     step h (parComp t1 t2) h' (parComp t1 t2')
   |BindStep : forall t1 t1' t2 h h', step h (thread t1) h' (thread t1') ->
-                                     step h (thread (bind t1 t2)) h' (thread (bind t1' t2))
+                                     step h (thread (bind t1 t2)) h' 
+                                          (thread (bind t1' t2))
   |Bind : forall t1 t2 h, step h (thread(bind (ret t1) t2)) h (thread (app t2 t1))
-  |Fork : forall t1 h, step h (thread (fork t1)) h (parComp (thread (ret unit)) (thread t1))
+  |Fork : forall t1 h, step h (thread (fork t1)) h 
+                            (parComp (thread (ret unit)) (thread t1))
+  |Get : forall  h i M,  h i = Some(full M) -> 
+                         step h (thread (get i)) h (thread (ret M))
+  |Put : forall h h' i M, h i = Some empty ->
+                       h' = extend h i (full M) ->
+                       step h (thread (put i M)) h' (thread (ret unit))
+  |New : forall h h' i, h i = None -> h' = extend h i empty ->
+                        step h (thread new) h' (thread (ret (var i)))
 .
+
+Inductive finished : pool -> Prop :=
+  |DoneT1 : forall T1 T2, finished T1 -> finished (parComp T1 T2)
+  |DoneT2 : forall T1 T2, finished T2 -> finished (parComp T1 T2)
+  |DoneT : forall M : term, finished (thread (done M)). 
+
 
 
 
