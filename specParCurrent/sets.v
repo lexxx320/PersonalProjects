9907add22adc12a11f9531c3d66dd6a3016943d8
Matthew Le <lexxx320@umn.edu>
2014-04-08 13:41:43 -0400

    updating

--- specParCurrent/sets.v
+++ specParCurrent/sets.v
@@ -84,5 +84,55 @@ destruct y_in_subtract. auto. inversion y_is_x. subst y. auto.
 Qed.
 
 
+Theorem eqImpliesSameSet : forall (T:Type) T1 T2, T1 = T2 -> Same_set T T1 T2. 
+Proof.
+  intros. unfold Same_set. unfold Included. split; intros. 
+  {subst. assumption. }
+  {subst. assumption. }
+Qed. 
+
+Ltac unfoldSetEq :=
+  match goal with
+      |H : ?S1 = ?S2 |- _ => try apply eqImpliesSameSet in H; unfold Same_set in H;
+                             unfold Included in *; inversion H
+  end.
+
+
+Hint Unfold In. 
+Hint Constructors Singleton Couple. 
+
+Theorem SingleEqCouple : forall (T:Type) s1 s2 s3,
+                           Singleton T s1 = Couple T s2 s3 -> s1 = s2 /\ s1 = s3.
+Proof.
+  intros. apply eqImpliesSameSet in H. unfold Same_set in H.
+  unfold Included in H. inversion H. split.
+  {assert(In T (Couple T s2 s3) s2). auto. apply H1 in H2.
+   inversion H2. reflexivity. }
+  {assert(In T (Couple T s2 s3) s3). auto. apply H1 in H2.
+   inversion H2. reflexivity. }
+Qed.
+
+
+Ltac invertSetEq := 
+  match goal with
+      |H : Empty_set ?T = Singleton ?T ?e |- _ => 
+       apply eqImpliesSameSet in H; unfold Same_set in H; unfold Included in H;
+       inversion H as [sameSet1 sameSet2]; 
+       assert(Hin:In T (Singleton T e) e) by auto;
+       apply sameSet2 in Hin; inversion Hin
+      |H : Empty_set ?T = Couple ?T ?e1 ?e2 |- _ =>
+       apply eqImpliesSameSet in H; unfold Same_set in H; unfold Included in H;
+       inversion H as [sameSet1 sameSet2];
+       assert(Hin:In T (Couple T e1 e2) e1) by auto; apply sameSet2 in Hin;
+       inversion Hin
+      |H:Singleton ?T ?e = Empty_set ?T |- _ => symmetry in H; invertSetEq
+      |H:Couple ?T ?e1 ?e2 = Empty_set ?T |- _ => symmetry in H; invertSetEq
+      |H:Singleton ?T ?e1 = Couple ?T ?e2 ?e3 |- _ => apply SingleEqCouple in H
+      |H:Couple ?T ?e2 ?e3 = Singleton ?T ?e1 |- _ => symmetry in H; invertSetEq
+  end. 
+
+
+
+
 End Ensembles.
 
