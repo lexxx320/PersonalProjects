41e6bea217eec1668eb2bd0650fc5521b84e853e
Matthew Le <ml9951@gol3571-02.casci.rit.edu>
2015-02-20 17:28:40 -0500

    updating

--- graphics/cg-fill/c++/Rasterizer.cpp
+++ graphics/cg-fill/c++/Rasterizer.cpp
@@ -10,8 +10,17 @@
 
 #include "Rasterizer.h"
 #include "simpleCanvas.h"
+#include <list>
 
 using namespace std;
+
+typedef struct{
+    int y;
+    int x;
+    int dx;
+    int dy;
+    int sum;
+} bucket;
 
 /**
  *
@@ -30,6 +39,59 @@ Rasterizer::Rasterizer (int n) : n_scanlines (n)
 {
 }
 
+
+list<bucket*> filter(list <bucket*> ael, int y){
+    for (list<bucket *>::iterator it = ael.begin(); it != ael.end(); it++){
+        bucket * b = *it;
+        if(b-> y == y){
+            ael.erase(it);
+        }
+    }
+    return ael;
+}
+
+void updateXs(list <bucket*>ael){
+    for (list<bucket *>::iterator it = ael.begin(); it != ael.end(); it++){
+        bucket * b = *it;
+        int xInc = (b->dx + b->sum) / b->dy;
+        b->sum = (b->dx + b->sum) % b->dy;
+        b->x += xInc;
+    }
+}
+
+bool lineLT(bucket * b1, bucket * b2){
+    if(b1->x < b2->x){
+        return true;
+    }else if (b1->x == b2->x){
+        double s1 = (double)b1->dx / (double) b1->dy;
+        double s2 = (double)b2->dx / (double) b2->dy;
+        return s1 < s2;
+    }else{
+        return false;
+    }
+}
+
+list<bucket*> insert(list<bucket*> yBucket, list<bucket*> ael){
+    for(list<bucket *>::iterator it = yBucket.begin(); it != yBucket.end(); it++){
+        bucket * yb = *it;
+        if(yb->dy == 0)
+            continue;
+        bool inserted = false;
+        for(list<bucket *>::iterator it2 = ael.begin(); it2 != ael.end(); it2++){
+            if(lineLT(yb, *it2)){
+                ael.insert(it2, yb);
+                inserted = true;
+                break;
+            }
+        }
+        if(!inserted){
+            ael.push_back(yb);
+        }
+    }
+    return ael;
+}
+
+
 /**
  * Draw a filled polygon in the simpleCanvas C.
  *
@@ -40,7 +102,78 @@ Rasterizer::Rasterizer (int n) : n_scanlines (n)
  * You are to add the implementation here using only calls
  * to C.setPixel()
  */
-void Rasterizer::drawPolygon(int n, int x[], int y[], simpleCanvas &C)
-{
-    // YOUR IMPLEMENTATION GOES HERE
+void Rasterizer::drawPolygon(int n, int x[], int y[], simpleCanvas &C){
+    list <bucket *> * el = new list<bucket *>[n_scanlines]; //edge table
+    int j;
+
+    //Build edge table
+    for(int i = 0; i < n; i++){
+        j = (i + 1) % n;
+        bucket * b = (bucket*)malloc(sizeof(bucket));
+        if(y[i] < y[j]){
+            b->y = y[j];
+            b->x = x[i];
+            el[y[i]].push_front(b);
+        }else{
+            b->y = y[i];
+            b->x = x[j];
+            el[y[j]].push_front(b);
+        }
+        b->dx = x[j] - x[i];
+        b->dy = y[j] - y[i];
+        b->sum = 0;
+    }
+    
+    list<bucket *> ael;
+    
+    for(int y = 0; y < n_scanlines; y++){
+        ael = filter(ael, y);
+        updateXs(ael);
+        list <bucket*> yBucket = el[y];
+        //printf("before: ael size is %lu, yBucket size is %lu\n", ael.size(), yBucket.size());
+        ael = insert(yBucket, ael);
+        //printf("after: ael size is %lu\n", ael.size());
+        for(list<bucket *>::iterator it = ael.begin(); it != ael.end(); it++){
+            bucket * e1 = *it;
+            it++;
+            if(it == ael.end()){
+                it = ael.begin();
+                bucket * e2 = *it;
+                int start = e1->sum == 0 ? e1->x : e1->x+1;
+                int end = e2->sum == 0 ? e2->x-1 : e2->x;
+                for(int x = start; x <= end; x++){
+                    C.setPixel(x, y);
+                }
+            }else{
+                bucket * e2 = *it;
+                int start = e1->sum == 0 ? e1->x : e1->x+1;
+                int end = e2->sum == 0 ? e2->x-1 : e2->x;
+                for(int x = start; x <= end; x++){
+                    C.setPixel(x, y);
+                }
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
